9d5063fed23a056d58ab667fbd332d4d3e211db3
Alexander <alex.albasov@gmail.com>
2023-04-04 12:32:16 +0700

    program has been created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int balance = 100;
+        int payment = 1100;
+        int bonus = 0;
+        if (payment > 1000) {
+            bonus = payment / 100;
+        }
+        balance = balance + payment + bonus;
+        System.out.println("Итоговый счет: " + balance);
+        System.out.println("Бонусные рубли: " + bonus);
     }
 }
